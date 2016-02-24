9040d3c0da75e22c338df38b65a64995da5d3d72
Xin Zhou <xinzhou1023@gmail.com>
2016-02-24 16:55:35 -0500

    init

---
+++ admin/opt/admin/run_opt_admin.r
@@ -0,0 +1,24 @@
+setwd("/home/rstudio/nviz/kohls/admin/opt/admin/")
+suppressMessages(suppressWarnings(library(RMySQL)))
+# True generate db version files, F generates flat file version files
+db.usage=T
+# True is to staging DB and F is to production DB
+is.staging=F 
+# DB server info
+db.server="bitnami.cluster-chdidqfrg8na.us-east-1.rds.amazonaws.com"
+db.name="nviz"
+port=3306
+if (is.staging){
+  username="root"
+  password="bitnami"
+}else{
+  username="Zkdz408R6hll"
+  password="XH3RoKdopf12L4BJbqXTtD2yESgwL$fGd(juW)ed"
+}
+main.path="/home/rstudio/nviz/opt/" # opt files path
+season.var=0.05  # from 0 to 1
+
+
+if (db.usage) conn <- dbConnect(MySQL(),user=username, password=password,dbname=db.name, host=db.server)
+source(paste(main.path,"adm_main.r",sep=""),local=T)
+if (db.usage) dbDisconnect(conn)
---
+++ admin/opt/curve/fit curves.R
@@ -0,0 +1,7 @@
+library(data.table);library(bit64)
+setwd("c:\\Users\\XinZhou\\Documents\\GitHub\\kohls\\admin\\opt\\curve\\")
+learn.rate.start=1e-6 # the start point of learn rate for fitting curves (vary from curve file to curve file)
+for.opt=T # T if the fitting is for otptimization curves
+is.refit=F # refit with a/b curve or use the adstock formula
+
+source("c:\\Users\\XinZhou\\Documents\\GitHub\\curve-simulation\\curve simulator from ad.R")
