0e2ee6c6564791f809c1423de85b36bb6605b4e7
Xin Zhou <xinzhou1023@gmail.com>
2016-04-06 12:04:48 -0400

    update curve fit code

--- admin/opt/curve/fit curves.R
+++ admin/opt/curve/fit curves.R
@@ -4,4 +4,7 @@ learn.rate.start=1e-6 # the start point of learn rate for fitting curves (vary f
 for.opt=T # T if the fitting is for otptimization curves
 is.refit=F # refit with a/b curve or use the adstock formula
 
+ex.curve=fread("sim_modelinput_curve.csv")
+ex.dim=fread("sim_input_setup.csv")
+
 source("c:\\Users\\XinZhou\\Documents\\GitHub\\curve-simulation\\curve simulator from ad.R")
