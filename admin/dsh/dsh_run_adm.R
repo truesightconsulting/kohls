299265756da7508bbfe1fb6574a38242501fc7c9
yoruhyde <yuemeng1@USNYCZOG8351.global.publicisgroupe.net>
2016-04-08 15:11:07 -0400

    delete dma lookup add map market

--- admin/dsh/dsh_run_adm.R
+++ admin/dsh/dsh_run_adm.R
@@ -7,7 +7,7 @@ path_system="C:\\Users\\yuemeng1\\"
 # folder where you put your key for AWS RDS
 path_key="C:\\Users\\yuemeng1\\Desktop\\Github\\ninah.ppk"
 # folder where you put all input files for the client
-path_client="C:\\Users\\yuemeng1\\Desktop\\code\\dashboard\\Kohls_1\\" #ensure there is \\ at the end of the path
+path_client="C:\\Users\\yuemeng1\\Desktop\\code\\dashboard\\kohls\\admin\\dsh\\" #ensure there is \\ at the end of the path
 # folder where you put the main code
 path_R="C:\\Users\\yuemeng1\\Desktop\\code\\dashboard\\R"
 
@@ -18,7 +18,7 @@ setwd(path_client)
 final=fread("dsh_modelinput_data.csv")
 datelkup=fread("dsh_input_lookup_date.csv")
 varlkup=fread("dsh_input_lookup_var.csv",na.strings="")
-dmalkup=fread("dsh_input_lookup_dma.csv")
+# dmalkup=fread("dsh_input_lookup_dma.csv")
 setin=fread("dsh_input_setup_drilldown.csv",na.strings="")
 homesetup=fread("dsh_input_setup_home.csv")
 typetable=fread("dsh_input_type.csv")
@@ -28,7 +28,7 @@ setup=fread("dsh_input_setup.csv")
 
 
 #Please check if the date format makes sense after this step
-final$week=as.Date(final$week)
+final$week=as.Date(final$week,"%m/%d/%Y")
 
 if(setup$update ==1) update=T else update=F
 if(setup$is.staging==1) is.staging=T else is.staging=F
