ec59a7aec35541741ac3c005601852d0596279c2
Xin Zhou <xinzhou1023@gmail.com>
2016-05-10 12:05:29 -0400

    drop beta.decomp code

--- opt_input_curve_par.r
+++ opt_input_curve_par.r
@@ -1,15 +1,12 @@
 # set beta based on clv value
 if (curve_f=="ninah") {
-  curve$beta.decomp=curve$beta
   if(ex.setup$optimization_target==2){
     curve=curve[,beta:=beta*clv]
   }else if (ex.setup$optimization_target==3){
     curve=curve[,beta:=beta/factor_1]
   }
-}else if (curve_f %in% c("exp","exp000")){
-  curve$a.decomp=curve$a
+}else if (curve_f %in% c("exp","exp000","exp_cpm")){
   if(ex.setup$optimization_target==2){
     curve=curve[,a:=a*clv]
   }
 }
-
